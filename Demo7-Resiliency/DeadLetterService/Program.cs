295d10bad76fcb107763631bbf84f0944cb98b9c
Marc Duiker <marcduiker@users.noreply.github.com>
2025-08-22 18:16:31 +0200

    Start with resiliency demo
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

---
+++ Demo7-Resiliency/DeadLetterService/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+app.UseCloudEvents();
+
+app.MapPost("/messagehandler", (
+    TinyMessage message) =>
+{
+    Console.WriteLine($"Received deadletter message {message.Id}.");
+
+    return Results.Accepted();
+});
+
+app.Run();
+
+record TinyMessage(string Id, DateTime TimeStamp, string Type, int Amount = 0);
