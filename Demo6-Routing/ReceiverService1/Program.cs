89b42deb2d1f58b990e320afb46d4b0e917097b4
Marc Duiker <marcduiker@users.noreply.github.com>
2025-08-22 16:54:40 +0200

    Add second receiver service to routing example with declarative route rules
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- Demo6-Routing/ReceiverService1/Program.cs
+++ Demo6-Routing/ReceiverService1/Program.cs
@@ -10,22 +10,13 @@ app.MapSubscribeHandler();
 const string PUBSUB_NAME = "demo6-pubsub";
 const string TOPIC_NAME = "incoming-messages";
 const string ROUTE_TYPE1 = "event.data.type == \"dapr.demo.type1\"";
-const string ROUTE_TYPE2 = "event.data.type == \"dapr.demo.type2\"";
 const string ROUTE_TYPE1_LARGEAMOUNT = "event.data.type == \"dapr.demo.type1\" && event.data.amount > 10";
 const int PRIORITY100 = 100;
 const int PRIORITY200 = 200;
-const int PRIORITY300 = 300;
 
 app.MapPost("/handletype1", [Topic(PUBSUB_NAME, TOPIC_NAME, ROUTE_TYPE1, PRIORITY200)](
     TinyMessage message) => {
     Console.WriteLine($"Type1 - Received message {message.Id}: {message.Type}.");
-
-    return Results.Accepted();
-});
-
-app.MapPost("/handletype2", [Topic(PUBSUB_NAME, TOPIC_NAME, ROUTE_TYPE2, PRIORITY300)](
-    TinyMessage message) => {
-    Console.WriteLine($"Type2 - Received message {message.Id}: {message.Type}.");
 
     return Results.Accepted();
 });
---
+++ Demo6-Routing/ReceiverService2/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+app.UseCloudEvents();
+
+app.MapPost("/messagehandler", (
+    TinyMessage message) =>
+{
+    Console.WriteLine($"Received message {message.Id}.");
+
+    return Results.Accepted();
+});
+
+app.MapPost("/handletype2", (
+    TinyMessage message) => {
+    Console.WriteLine($"Type2 - Received message {message.Id}: {message.Type}.");
+
+    return Results.Accepted();
+});
+
+app.Run();
+
+record TinyMessage(string Id, DateTime TimeStamp, string Type, int Amount = 0);
