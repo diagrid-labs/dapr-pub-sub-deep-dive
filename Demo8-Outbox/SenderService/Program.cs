b7a38e04ff4a76d0b6edccc30f6ff3ff1222b5bd
Marc Duiker <marcduiker@users.noreply.github.com>
2025-09-01 20:18:54 +0000

    Update Outbox pattern
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- Demo8-Outbox/SenderService/Program.cs
+++ Demo8-Outbox/SenderService/Program.cs
@@ -22,18 +22,6 @@ app.MapPost("/save", async (
             }
         );
 
-        // Define the second state operation to publish the value "3" with metadata
-        // var metadata = new Dictionary<string, string>
-        // {
-        //     { "outbox.projection", "true" }
-        // };
-        // var op2 = new StateTransactionRequest(
-        //     key: "key1",
-        //     value: Encoding.UTF8.GetBytes("3"),
-        //     operationType: StateOperationType.Upsert,
-        //     metadata: metadata
-        // );
-
         // Create the list of state operations
         var operations = new List<StateTransactionRequest> { request };
 
@@ -41,7 +29,7 @@ app.MapPost("/save", async (
         await daprClient.ExecuteStateTransactionAsync(StateComponentName, operations);
         Console.WriteLine($"State transaction executed {message.Id}.");
 
-        return Results.Created(message.Id, value: null);
+        return Results.Created(string.Empty, message.Id);
     }
 );
 
